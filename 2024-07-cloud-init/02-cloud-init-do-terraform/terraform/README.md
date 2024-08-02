e9734273690f1862bc2ec200b14b7374a65fd270
Alex Kretzschmar <2773080+ironicbadger@users.noreply.github.com>
2024-08-02 11:57:56 -0400

    2024 07 cloud init (#2)
    * cloud-init wip
    
    * gitignore
    
    * gitignore for tf
    
    * add ansible stuff
    
    * first working prototype
    
    * exit node
    
    * template authkey
    
    * ssh key hint
    
    * example tfvars
    
    * extra style points

---
+++ 2024-07-cloud-init/02-cloud-init-do-terraform/terraform/README.md
@@ -0,0 +1,11 @@
+
+
+## Get SSH key ID number
+
+```
+$ doctl -t dop_v1_d912349ce88 compute ssh-key list
+
+ID          Name          FingerPrint
+41133189    laptop        d0:3b:39:f0:61:f7:f5:ad:9a:d9:ee:44:92:09:5f:34
+22222222    dev           2a:ad:9a:d9:ca:9c:77:ee:44:92:09:5f:3f:a7:6f:da
+```
---
+++ 2024-07-cloud-init/02-cloud-init-do-terraform/terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+variable "do_token" {}
+variable "tailscale_auth_key" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "droplet" {
+  image     = "ubuntu-24-04-x64"
+  name      = "nyc1-terraform-caddy"
+  region    = "nyc1"
+  size      = "s-1vcpu-1gb"
+  # doctl -t dop_v1_token compute ssh-key list
+  ssh_keys  = [41743189]
+  user_data = templatefile("digitalocean.tftpl", { tailscale_auth_key = var.tailscale_auth_key })
+}
