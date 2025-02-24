495b1fb969a73880f994391d9051aca824ff4cc8
Alex Kretzschmar <alexktz@gmail.com>
2025-02-24 15:22:02 -0500

    updated hardware

--- 2025-02-nix-nvidia-ollama/hosts/nixos/nix-llm/hardware-configuration.nix
+++ 2025-02-nix-nvidia-ollama/hosts/nixos/nix-llm/hardware-configuration.nix
@@ -8,10 +8,23 @@
     [ (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/2cdef900-1872-4295-bbc1-c199bc67027c";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/12CE-A600";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
