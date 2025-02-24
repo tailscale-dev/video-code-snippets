ffa4bd4e84904bd3eb9222a87139c0b627724b3c
Alex Kretzschmar <alexktz@gmail.com>
2025-02-24 15:50:14 -0500

    update

--- 2025-02-nix-nvidia-ollama/hosts/nixos/nix-llm/hardware-configuration.nix
+++ 2025-02-nix-nvidia-ollama/hosts/nixos/nix-llm/hardware-configuration.nix
@@ -27,7 +27,7 @@
     };
   swapDevices = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # Enabless DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
