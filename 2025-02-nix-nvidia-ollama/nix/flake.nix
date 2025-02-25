d2738bdf6e3ef6e95d55e175985b3da8ad4197d6
Alex Kretzschmar <alexktz@gmail.com>
2025-02-24 21:17:25 -0500

    tidy up

--- 2025-02-nix-nvidia-ollama/nix/flake.nix
+++ 2025-02-nix-nvidia-ollama/nix/flake.nix
@@ -2,9 +2,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # disko.url = "github:nix-community/disko";
-    # disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { ... }@inputs:
@@ -12,11 +9,8 @@
     let
       inherit (self) outputs;
       stateVersion = "24.11";
-      #pkgs = system: import nixpkgs { inherit system; config.allowUnfree = true; };
       libx = import ./lib { inherit inputs outputs stateVersion pkgs; };
-
     in {
-
       nixosConfigurations = {
         nix-llm = libx.mkNixos {
           system = "x86_64-linux";
@@ -24,6 +18,5 @@
           username = "zaphod";
         };
       };
-
    };
 }
